08a02899cceceb5f2f4217319a6edf2d65c66953
Your Username <you@example.com>
2021-02-03 22:16:20 -0500

    Done.

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 document.querySelector("button").addEventListener("click", handleClick);
 
 function handleClick(e) {
-  
+  const timeString = document.getElementById('time').value
   displayMessage(greet(timeString))
 }
 
